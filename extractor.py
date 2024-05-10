5c3538e07bc93d40db99dc23c17789b265dcda1b
RenanGroot <36904185+RenanGroot@users.noreply.github.com>
2024-05-09 23:44:32 -0300

    created check_next_page

--- extractor.py
+++ extractor.py
@@ -2,3 +2,49 @@
 # THis function is going to extract the data from the site to the sql database
 # https://www.tbca.net.br/index.html
 
+import requests
+import pandas as pd
+
+
+def check_next_page(
+        html:str
+) -> bool:
+    """
+    Args: 
+        html(str): page's html
+    
+    Returns:
+        bool: True(if there is next page link); False(if there isn't next page link)
+    """
+
+    if ' class="nav-link">próxima' in html:
+        return True
+    else:
+        return False
+
+def extract_table(
+        target_url:str
+) -> pd.DataFrame():
+    """
+    Extract the table contained in the html given the target page's url.
+
+    Args:
+        target_url(str): Target page url
+    
+    Returns:
+        pd.DataFrame: Dataframe containing table data
+    """
+
+    response = requests.get(target_url)
+    response_html = response.text
+
+    print(check_next_page(response_html))
+
+    # Taking table
+    tables_html = pd.read_html(response_html)
+    df = tables_html[0]
+    return df
+
+
+dtframe = extract_table("https://www.tbca.net.br/base-dados/composicao_estatistica.php")
+print(dtframe)
