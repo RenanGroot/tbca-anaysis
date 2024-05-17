31771566279c142b39fa59de75fb93d86050e780
RenanGroot <36904185+RenanGroot@users.noreply.github.com>
2024-05-17 00:19:11 -0300

    create landing_to_sql

---
+++ landing_to_sql.py
@@ -0,0 +1,52 @@
+import sqlite3
+import subprocess
+
+def create_database():
+    """
+    Creates the database (if not exists), inside folder `database/`.
+    """
+    connection = sqlite3.connect("database/foods.db")
+    
+    cursor = connection.cursor()
+
+    # Creating Tables, if not exist
+    cursor.executescript(
+    """
+    CREATE TABLE IF NOT EXISTS food_list (
+        id TEXT PRIMARY KEY,
+        name TEXT,
+        scientific_name TEXT,
+        group TEXT,
+        brand TEXT );
+
+    CREATE TABLE IF NOT EXISTS food_details (
+        id TEXT PRIMARY KEY,
+        name TEXT,
+        scientific_name TEXT,
+        group TEXT,
+        brand TEXT );
+    """
+    )
+    connection.commit()
+
+
+
+def upload_csv(csv_path:str) -> None:
+    """
+    This function creates the database (if not exists), and populate it with a csv file.
+
+    Args:
+        csv_path (str): Csv file path. Ex: "landing_files/food_table/food_page1.csv"
+  
+    """
+    
+    # Run subprocess for populating the DB with the csv file
+    subprocess.run(['sqlite3',
+                         str("database/foods.db"),
+                         '-cmd',
+                         '.mode csv',
+                         '.import --skip 1 ' + csv_path
+                                 +' flights'],
+                        capture_output=True)
+
+    print(f"Successfully populated db with data from : {csv_path}")
